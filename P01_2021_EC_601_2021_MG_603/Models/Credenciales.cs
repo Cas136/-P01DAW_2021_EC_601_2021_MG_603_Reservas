6bd358de46eb8fc801957cbfda5dd3c05be1668b
Cas136 <bryancastaneda999@gmail.com>
2025-02-27 20:47:28 -0600

    Update Credenciales.cs

--- P01_2021_EC_601_2021_MG_603/Models/Credenciales.cs
+++ P01_2021_EC_601_2021_MG_603/Models/Credenciales.cs
@@ -1,16 +1,8 @@
 namespace P01_2021_EC_601_2021_MG_603.Models
 {
-    public class Reservas
+    public class Credenciales
     {
-        public int ReservaId { get; set; }
-        public int UsuarioId { get; set; }
-        public int EspacioId { get; set; }
-        public DateTime FechaReserva { get; set; }
-        public TimeSpan HoraReserva { get; set; }
-        public int CantidadHoras { get; set; }
-        public string EstadoReserva { get; set; }
-        public Usuarios Usuario { get; set; }
-        public EspaciosParqueo EspaciosParqueo { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
     }
 }
-
