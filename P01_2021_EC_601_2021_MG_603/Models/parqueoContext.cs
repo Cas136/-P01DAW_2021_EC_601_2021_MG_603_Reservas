afaa730ad670e8dde5943d6eee1afbe4233b22e2
Cas136 <bryancastaneda999@gmail.com>
2025-02-27 20:28:54 -0600

    Update parqueoContext.cs

--- P01_2021_EC_601_2021_MG_603/Models/parqueoContext.cs
+++ P01_2021_EC_601_2021_MG_603/Models/parqueoContext.cs
@@ -1,43 +1,43 @@
 using Microsoft.EntityFrameworkCore;
 using P01_2021_EC_601_2021_MG_603.Models;
-
+using P01_2021_EC_601_2021_MG_603.Data;
 namespace P01_2021_EC_601_2021_MG_603.Data
 {
     public class parqueoContext : DbContext
     {
         public parqueoContext(DbContextOptions<parqueoContext> options) : base(options) { }
 
-        public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<Sucursal> Sucursales { get; set; }
-        public DbSet<EspacioParqueo> EspaciosParqueo { get; set; }
-        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Sucursales> Sucursales { get; set; }
+        public DbSet<EspaciosParqueo> EspaciosParqueo { get; set; }
+        public DbSet<Reservas> Reservas { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Usuario>()
+            modelBuilder.Entity<Usuarios>()
                 .HasIndex(u => u.Correo)
                 .IsUnique();
 
-            modelBuilder.Entity<Sucursal>()
-                .HasOne<Usuario>()
+            modelBuilder.Entity<Sucursales>()
+                .HasOne<Usuarios>()
                 .WithMany()
                 .HasForeignKey(s => s.AdministradorId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<EspacioParqueo>()
-                .HasOne<Sucursal>()
+            modelBuilder.Entity<EspaciosParqueo>()
+                .HasOne<Sucursales>()
                 .WithMany()
                 .HasForeignKey(e => e.SucursalId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<Reserva>()
-                .HasOne<Usuario>()
+            modelBuilder.Entity<Reservas>()
+                .HasOne<Usuarios>()
                 .WithMany()
                 .HasForeignKey(r => r.UsuarioId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<Reserva>()
-                .HasOne<EspacioParqueo>()
+            modelBuilder.Entity<Reservas>()
+                .HasOne<EspaciosParqueo>()
                 .WithMany()
                 .HasForeignKey(r => r.EspacioId)
                 .OnDelete(DeleteBehavior.Cascade);
